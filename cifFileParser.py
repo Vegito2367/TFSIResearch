da01251cfb240f91eda7245819d950ff3a90357c
Vegito2367 <tejaaditya.g@gmail.com>
2024-02-22 18:15:55 -0600

    Made the file parser a class

--- cifFileParser.py
+++ cifFileParser.py
@@ -3,21 +3,35 @@ import matplotlib.pyplot as plt
 from atom import Atom
 import os
 
-def printNicely(myList):
-  for j in myList:
-    print(j)
+class CIFParser:
+  def printNicely(myList):
+    for j in myList:
+      print(j)
 
-curDir=r"C:\Users\ASUS\Desktop\ProfJamesResources\testcif"
-filename="AKONEU.cif"
-allfiles=os.listdir(curDir)
+  def __init__(self,filePath):
+
+    myfile=open(filePath,'r')
+
+    alllines=myfile.read().split("\n")
+    textofInterest=alllines[alllines.index("_atom_site_fract_z")+1:alllines.index("#END")]
+
+    self.Atoms=[]
+    for j in textofInterest:
+      self.Atoms.append(Atom(j.split(" ")))
+
+    self.nitrogens=next((i for i,atom in enumerate(self.Atoms) if atom.symbol == "N"),None)
+    self.sulphurs=next((i for i,atom in enumerate(self.Atoms) if atom.symbol == "S"),None)
+    print(next((i for i,atom in enumerate(self.Atoms) if atom.symbol == "H"),None))
+
+  def getElementAtoms(self,symbol):
+    output=[]
+    for j in range(len(self.Atoms)):
+      if(self.Atoms[j].symbol == symbol):
+        output.append(j)
+
+    return output
 
 
-myfile=open(f"{curDir}\{allfiles[0]}",'r')
+parser=CIFParser("testcif\AKONEU.cif")
 
-alllines=myfile.read().split("\n")
-textofInterest=alllines[alllines.index("_atom_site_fract_z")+1:alllines.index("#END")]
-
-allAtoms=[]
-for j in textofInterest:
-  allAtoms.append(Atom(j.split(" ")))
-
+print(parser.getElementAtoms("N"))
