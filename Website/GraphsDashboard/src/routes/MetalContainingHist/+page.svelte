91c43639f1c35575a9afb5e3c0e78d5c01b6b9b5
Vegito2367 <tejaaditya.g@gmail.com>
2024-10-04 17:47:30 -0500

    Fixed Sub and super scripts

--- Website/GraphsDashboard/src/routes/MetalContainingHist/+page.svelte
+++ Website/GraphsDashboard/src/routes/MetalContainingHist/+page.svelte
@@ -4,6 +4,6 @@
 <main style="margin: 15px;">
   <center><img src={hist} alt="Metal Presence" style="margin-bottom:10px;"></center>
   <p style="margin-left: 12%;margin-right: 12%;font-size: large;font-family:var(--font-mono); font-weight:600;">
-    Comparison of the number of metal-containing and metal-free structures. The percentage of the structures containing a metal was found to be 32.3%, while the percentage lacking a metal was 67.7%. The dataset for this comparison was obtained from the Cambridge Structual Database by searching for all structures featuring the TFSI core framework (which has the molecular formula N(SO2CF3)2–) as specified by connectivity only and agnostic of the individual atom-to-atom bond type. 
+    Comparison of the number of metal-containing and metal-free structures. The percentage of the structures containing a metal was found to be 32.3%, while the percentage lacking a metal was 67.7%. The dataset for this comparison was obtained from the Cambridge Structual Database by searching for all structures featuring the TFSI core framework (which has the molecular formula N(SO<sub>2</sub>CF<sub>3</sub>)<sup>2-</sup>) as specified by connectivity only and agnostic of the individual atom-to-atom bond type. 
   </p>
 </main>
