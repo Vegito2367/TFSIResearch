b0d07e5b4f20dbeee2780cb457c02bc319229c02
Vegito2367 <tejaaditya.g@gmail.com>
2024-10-03 21:16:34 -0500

    Removed HTML files

--- Website/GraphsDashboard/src/routes/dynamic/+page.svelte
+++ Website/GraphsDashboard/src/routes/dynamic/+page.svelte
@@ -1,10 +1,3 @@
-<script>
-  import graph from "$lib/images/github.svg";
-</script>
-
 <main>
-  <!-- svelte-ignore a11y-missing-attribute -->
-  Hello world
-  <iframe src="/AgPres.html" width="100%" height="100%" frameborder="0"></iframe>
-  <img src="{graph}" width="100%" height="100%" alt="GitHub Logo" />
+    Hello world
 </main>
